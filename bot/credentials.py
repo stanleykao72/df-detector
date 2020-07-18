ec74c2a6ccff1987487e7a01c15444185054e210
Stanley Kao <stanleykao72@gmail.com>
2020-07-18 14:35:31 +0800

    update security setting

--- bot/credentials.py
+++ bot/credentials.py
@@ -1,3 +1,6 @@
-bot_token = '1168941932:AAGiYEV79cjssoQX_rZ5IwE4nbFhliKlh5M'
-bot_user_name = 'imagedetect_bot'
-URL = "https://dfdetector.herokuapp.com/"
+# bot_token = '1168941932:AAGiYEV79cjssoQX_rZ5IwE4nbFhliKlh5M'
+bot_token = os.getenv("bot_token")
+# bot_user_name = 'imagedetect_bot'
+bot_user_name = os.getenv("bot_user_name")
+# URL = "https://dfdetector.herokuapp.com/"
+URL = os.getenv("URL")
