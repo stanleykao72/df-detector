ef0f5bad4bce5a208a5d3eb9d098e64363305aaf
Stanley Kao <stanleykao72@gmail.com>
2020-07-19 13:29:28 +0800

    recover to original

--- app.py
+++ app.py
@@ -71,8 +71,9 @@ def echo(update, context):
      簡稱自動回話，也就是你打什麼，他就回你什麼
     """
     text = update.message.text  # 取得對話的內容
-    # update.message.reply_text(text)  # 回覆你輸入的內容
-    context.bot.send_message(chat_id=update.effective_chat.id, text)
+    logger.info(f'text:{text}')
+    update.message.reply_text(text)  # 回覆你輸入的內容
+    logger.info(f'reply text')
 
 
 # Create a DLBot instance
