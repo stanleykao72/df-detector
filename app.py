1690ccc9fc1833ec71cd4c92a2c612fa53e1ac3c
Stanley Kao <stanleykao72@gmail.com>
2020-07-19 10:11:43 +0800

    recover to original

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 import telegram
 from telegram.ext import Dispatcher, CommandHandler, MessageHandler, Filters
 from bot.credentials import bot_token, bot_user_name, telegram_user_id, URL
-from bot.dl_bot import DLBot
+#from bot.dl_bot import DLBot
 import logging
 
 
@@ -11,10 +11,10 @@ global TOKEN
 TOKEN = bot_token
 
 # 機器人token
-# bot = telegram.Bot(token=TOKEN)
+bot = telegram.Bot(token=TOKEN)
 
 # Create a DLBot instance
-dlbot = DLBot(token=TOKEN, URL=URL, user_id=telegram_user_id)
+# dlbot = DLBot(token=TOKEN, URL=URL, user_id=telegram_user_id)
 
 # 印出log的方法
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -27,7 +27,7 @@ logger.setLevel(logging.INFO)
 # Initial Flask app
 app = Flask(__name__)
 
-'''
+
 # @app.route('/hook', methods=['POST'])
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
@@ -48,20 +48,10 @@ def respond():
         # bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
         dlbot.send_message(response)
     return 'ok'
-'''
 
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
-    s = dlbot.set_webhook()
-    if s:
-        dlbot.activate_bot()
-        return "webhook setup ok"
-    else:
-        return "webhook setup failed"
-
-
-'''
     # we use the bot object to link the bot to our app which live
     # in the link provided by URL
     s = bot.setWebhook('{URL}{HOOK}'.format(URL=URL, HOOK=TOKEN))
@@ -70,13 +60,13 @@ def set_webhook():
         return "webhook setup ok"
     else:
         return "webhook setup failed"
-'''
+
 
 @app.route('/')
 def index():
     return 'root directory'
 
-'''
+
 def echo(bot, update):
     """
      簡稱自動回話，也就是你打什麼，他就回你什麼
@@ -89,7 +79,6 @@ def echo(bot, update):
 dispatcher = Dispatcher(bot, None, use_context=True)
 echo_handler = MessageHandler(Filters.text, echo)  # 當你輸入 hi 機器人就會回你 hi
 dispatcher.add_handler(echo_handler)  # 也將剛剛自動回覆的功能加到你的 bot內
-'''
 
 if __name__ == '__main__':
     app.run(threaded=True)
